e8bc8f409e8306efae5960b4e1cd8d02852829c8
sara segura <sarasegura224@gmail.com>
2023-12-07 14:32:05 -0500

    commit

--- src/components/HomeLeft/index.jsx
+++ src/components/HomeLeft/index.jsx
@@ -8,7 +8,16 @@ export const HomeLeft = () => {
       <p className="HomeLeft-greeting">Hola,</p>
       <p className="HomeLeft-name">Mi nombre es Jhon y soy:</p>
       <p className="HomeLeft-position">Front-end Developer</p>
-      <p className="HomeLeft-descrition"></p>
+      <p className="HomeLeft-description">
+        🚀 Soy un profesional en mercadeo y desarrollador front-end con más de
+        un año de experiencia. Mis habilidades abarcan React, React Native,
+        Firebase, VITEX, TypeScript, y tecnologías web como Sass, JavaScript,
+        HTML, y CSS. Aprendí de manera autodidacta en Platzi, donde consolidé
+        mis conocimientos. Mi entusiasmo por nuevas tecnologías y la creación
+        continúa impulsando mi carrera. Si buscas colaborar en proyectos
+        emocionantes, contáctame. ¡Estoy emocionado por construir algo increíble
+        juntos! 🌟
+      </p>
       <Link to="/portafolio/projects">
         <button className="HomeLeft-button">Proyectos</button>
       </Link>
